0a3ebdd9c61d7ad8b399d2bff0cb2bf15efa3d32
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-29 00:20:46 +0400

    ajout param autoincrement

--- database/entity/colonnes.py
+++ database/entity/colonnes.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String
 class Colonnes(Base):
     __tablename__ = 'colonnes' # modif __ __
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     titreColonne = Column(String)
     pos = Column(Integer) # Ajouter contrainte d'unicité
 
