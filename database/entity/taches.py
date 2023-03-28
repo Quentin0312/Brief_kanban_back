811a56a1cab6ff16d0c91f94d46323873c9d5359
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-29 00:20:58 +0400

    ajout param autoincrement

--- database/entity/taches.py
+++ database/entity/taches.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 class Taches(Base):
     __tablename__ = 'taches'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     titreTaches = Column(String)
     id_colonne = Column(Integer, ForeignKey('colonnes.id'))
     pos = Column(Integer) #Ajouter contrainte d'unicité
