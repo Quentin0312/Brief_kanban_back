262634c1405057b38a9607d051497ef08931f843
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-29 00:28:13 +0400

    commentaire

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import hug
 from hug.middleware import CORSMiddleware
 from controllers import colonne, tache
 
-api = hug.API(__name__) # __xx__ ?
+api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api, allow_origins=['*'])) # allow_origins à restreindre pour le déploiement
 
 api.extend(colonne, '/api/colonne')
