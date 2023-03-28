1b05e97d358c7c326487eca94d3f4488d54f005d
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-28 21:00:48 +0400

    mise en place controller getColumn

--- controllers/colonne.py
+++ controllers/colonne.py
@@ -1,4 +1,6 @@
 import hug
+from database.database import session
+from database.entity import Colonnes
 
 @hug.post('/')
 def createColumn(body):
@@ -10,6 +12,26 @@ def modifyColumn(body):
     columnTitle = body['columnTitle']
     return columnTitle + " put"
 
-@hug.get('/{column_id}')
-def modifyColumn(column_id:int):
-    return str(column_id) + " get"
+# @hug.get('/{column_id}')
+# def getColumn(column_id:int):
+#     return str(column_id) + " get"
+
+# @hug.get('/')
+# def getColumn():
+#     queryColonne = session.query(Colonnes).order_by(Colonnes.pos).with_entities(Colonnes.titreColonne)
+#     listeColonne = []
+#     for elt in queryColonne:
+#         for subelt in elt:
+#             listeColonne.append(subelt)
+#     print("=================>",listeColonne)
+#     return listeColonne
+
+@hug.get('/')
+def getColumn():
+    queryColonne = session.query(Colonnes).order_by(Colonnes.pos).with_entities(Colonnes.id, Colonnes.titreColonne)
+    # listeColonne = []
+    # for elt in queryColonne:
+    #     for subelt in elt:
+    #         listeColonne.append(subelt)
+    # print("=================>",listeColonne)
+    return queryColonne
