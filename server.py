e5d710e25f9d82763e5f3e5c276e820ce883881e
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-28 11:12:45 +0400

    fix erreur CORS

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import hug
+from hug.middleware import CORSMiddleware
 from controllers import colonne, tache
-api = hug.API(__name__)
+
+api = hug.API(__name__) # __xx__ ?
+api.http.add_middleware(CORSMiddleware(api, allow_origins=['*'])) # allow_origins à restreindre pour le déploiement
 
 api.extend(colonne, '/api/colonne')
 # api.extend(tache, '/api/tache')
