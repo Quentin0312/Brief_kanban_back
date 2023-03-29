58a74298cdd6f0e27de3e02d4cce2671a7d6cc6e
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-29 22:20:29 +0400

    reorder colonne mis en place

--- controllers/colonne.py
+++ controllers/colonne.py
@@ -5,8 +5,10 @@ from sqlalchemy import desc
 
 # Enregistrement d'une nouvelle colonne
 @hug.post('/')
-def createColumn():
-    nouvelleColonne = Colonnes(titreColonne = "Nouvelle colonne")  # Valeur titreColonne peut etre transmis dans la requete entrante
+def createColumn(body):
+    nvleColonne = body['titreColonne']
+    pos = int(body['pos'])
+    nouvelleColonne = Colonnes(titreColonne = nvleColonne, pos = pos)  # Valeur titreColonne peut etre transmis dans la requete entrante
     # Pour la pos, d'abord recup la dernière pos puis +1 (eviter requete=> info doit être présent dans la requete entrante)
     session.add(nouvelleColonne)
     session.commit()
@@ -21,6 +23,34 @@ def createColumn():
 def getColumn():
     queryColonne = session.query(Colonnes).order_by(Colonnes.pos).with_entities(Colonnes.id, Colonnes.titreColonne)
     return queryColonne
+
+# Enregistrer les nvle pos des colonnes
+@hug.put('/reorder')
+def reorderColumn(body):
+    # Datas
+    idColonne = int(body['idColonne'])
+    nvlePos = int(body['nvlePos'])
+
+    # Vérif si nvle pos diff de l'ancienne
+    # Pos départ à récup dans BDD, possibilité depuis le front envoyer requete que si différence
+    anciennePos = session.query(Colonnes).filter(Colonnes.id == idColonne).with_entities(Colonnes.pos).scalar() 
+
+    if anciennePos != nvlePos:
+        # Colonne drop vers la droite
+        if anciennePos < nvlePos:
+            session.query(Colonnes).filter(anciennePos < Colonnes.pos, nvlePos >= Colonnes.pos).update({Colonnes.pos : Colonnes.pos - 1})
+            session.commit()
+        # Colonne drop vers la gauche
+        elif anciennePos > nvlePos:
+            session.query(Colonnes).filter(anciennePos > Colonnes.pos, nvlePos <= Colonnes.pos).update({Colonnes.pos : Colonnes.pos + 1})
+            session.commit()
+        # Enregistrer pos col déplacé
+        session.query(Colonnes).filter(Colonnes.id == idColonne).update({"pos" : nvlePos})
+        session.commit()
+        return "changed"
+    else:
+        return "no change"
+
 
 # ----------Brouillon-----------------
 
