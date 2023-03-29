4abebdeefce4ff1c929cec4913c6186564c15e91
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-29 20:12:54 +0400

    ajout variable baseUrl

--- server.py
+++ server.py
@@ -5,5 +5,7 @@ from controllers import colonne, tache
 api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api, allow_origins=['*'])) # allow_origins à restreindre pour le déploiement
 
-api.extend(colonne, '/api/colonne')
-api.extend(tache, '/api/tache')
+baseUrl = "/api"
+
+api.extend(colonne, baseUrl + '/colonne')
+api.extend(tache, baseUrl + '/tache')
