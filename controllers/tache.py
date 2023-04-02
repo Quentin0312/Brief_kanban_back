36f2a3d31cf23b2682af56f9ca1301603df82476
Quentin PAYET <quentinpayet1203@gmail.com>
2023-04-02 16:10:03 +0400

    ajout commentaire, requete vers BDD limité

--- controllers/tache.py
+++ controllers/tache.py
@@ -13,8 +13,10 @@ def getTask():
 # Modif titre d'une tache
 @hug.put('/{idTache}')
 def modifyTaskTitle(body, idTache):
-    print("============body>",body)
+    # Data
     newTitle = body['newTitle']
+
+    # Modification
     session.query(Taches).filter(Taches.id == int(idTache)).update({ Taches.titreTaches : newTitle })
     session.commit()
     return 'ok'
@@ -27,14 +29,15 @@ def addTask(body):
     idColonne = int(body['idColonne'])
     pos = int(body['pos'])
 
+    # Modification
     nouvelleTache = Taches(titreTaches = titreTache, id_colonne = idColonne, pos = pos)
     session.add(nouvelleTache)
     session.commit()
 
+    # Récup de l'id
     derId = session.query(Taches).order_by(desc(Taches.id)).with_entities(Taches.id).first()
     derId = derId[0]
 
-    print("========derId===========>",derId)
 
     return {"derId" : derId, 'titreTache' : titreTache, 'idColonne' : idColonne, 'pos' : pos}
 
@@ -48,41 +51,39 @@ def modifyColumnId(body):
     sourceColumnId = int(body['sourceColumnId'])
     posCibleTache = int(body['posCibleTache'])
 
-    # Modif id_colonne
-    if targetColumnId != sourceColumnId:
-        session.query(Taches).filter(Taches.id == taskId).update({"id_colonne": targetColumnId})
-        session.commit()
-
     # Récup precedante pos
     posPrecedante = session.query(Taches).filter_by(id = taskId).with_entities(Taches.pos).scalar()
     session.commit()
+    # Possiblité de récup depuis le front pour limiter les requete vers BDD
+
+    # Modif id_colonne
+    if targetColumnId != sourceColumnId:
+        session.query(Taches).filter(Taches.id == taskId).update({"id_colonne": targetColumnId})
+        # session.commit()
     
     # Si changement de colonne
     if targetColumnId != sourceColumnId:
         # Modifier pos des taches dans la colonne source
         session.query(Taches).filter(Taches.id_colonne == sourceColumnId, Taches.pos > posPrecedante).update({Taches.pos: Taches.pos - 1})
-        session.commit()
+
         # Modifier pos des taches dans la colonne cible
         session.query(Taches).filter(Taches.id_colonne == targetColumnId, Taches.pos >= posCibleTache).update({Taches.pos: Taches.pos + 1})
-        session.commit()
+        # session.commit()
     
     # Tache drop vers le haut sans changement de colonne
     elif posPrecedante > posCibleTache:
         session.query(Taches).filter(Taches.id_colonne == sourceColumnId, posPrecedante > Taches.pos , posCibleTache <= Taches.pos ).update({Taches.pos: Taches.pos + 1})
-        session.commit()
+        # session.commit()
 
     # Tache drop vers le bas sans changement de colonne
     elif posPrecedante < posCibleTache:
         session.query(Taches).filter(Taches.id_colonne == sourceColumnId, posPrecedante < Taches.pos , posCibleTache >= Taches.pos ).update({Taches.pos: Taches.pos - 1})
-        session.commit()
+        # session.commit()
 
     # Enregistrement nouvel pos
     session.query(Taches).filter(Taches.id == taskId).update({"pos": posCibleTache})
+    
+    # Commit de toutes les modifs
     session.commit()
 
     return 'ok'
-
-# @hug.get('/reorderTest')
-# def reorderTest():
-#     posPrecedante = session.query(Taches).filter_by(id = 2).first()
-#     return posPrecedante
